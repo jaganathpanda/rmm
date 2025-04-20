881fdd3169090b398ad0c05170e5ef4c46ee7db5
Jaganath Panda <jaganathpanda@Jaganaths-MacBook-Air.local>
2025-04-20 15:34:15 +0530

    registartion and otp page

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./RegisterPage.css"
+import "./RegisterPage.css";
 
 const RegistrationPage = () => {
   const [rmmFirstName, setRmmFirstName] = useState("");
